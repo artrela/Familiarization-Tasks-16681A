cfaf45256c016fa44628ee7933368bc707168048
artrela <trelaalec@gmail.com>
2023-02-03 12:33:41 -0500

    add velocity control

--- Task 2 - Sensors and Motor Control Lab/PID_IR/PID_Control/PID_Control.ino
+++ Task 2 - Sensors and Motor Control Lab/PID_IR/PID_Control/PID_Control.ino
@@ -1,16 +1,47 @@
-// https://www.youtube.com/watch?v=HRaZLCBFVDE
+// PID Control for Position: https://www.youtube.com/watch?v=HRaZLCBFVDE
+// PID Control for Speed: https://www.youtube.com/watch?v=HRaZLCBFVDE
 
-namespace Motor {
-  static int pos_update = 0;
-  static int pos_last = 0;
+// are we targeting pos/vel
+int target_mode =  1; // 0 for position, 1 for velocity
+// what is the value we are targeting
+int des = 60;
+// init prev time
+long t_last = micros();
+// init last error
+long e_last = 0;
+namespace Pos
+{
+  static int update = 0;
+  static int last = 0;
 
-  const float pos_kp = 50;
-  const float pos_kd = 49.5;
-  const float pos_ki = 0.00;
+  const float kp = 50;
+  const float kd = 49.5;
+  const float ki = 0.00;
 
 }
 
-namespace Pins {
+namespace Vel
+{
+  static float update = 0;
+  static float last = 0;
+  static float value = 0;
+  static float filtered = 0;
+
+  static float prevT = 0;
+  static float deltaT = 0;
+
+  // kp needs to be of a certain value or else
+  // the motor wont move at all  
+  const float kp = 5;
+  const float ki = 0.00;
+
+  static float e = 0;
+  static float e_i = 0;
+
+}
+
+namespace Pins
+{
   const int encA = 3;
   const int encB = 2;
   const int driverPin1 = 9;
@@ -18,21 +49,14 @@ namespace Pins {
   const int pwm = 11;
 }
 
-namespace axis {
+namespace axis
+{
   const int lower = 0;
   const int upper = 360;
 }
 
-// are we targeting pos/vel
-int target_mode = 0; // 0 for position, 1 for velocity
-// what is the value we are targeting
-int des = 160;
-// init prev time
-long t_last = micros();
-// init last error
-long e_last = 0;
-
-void setup() {
+void setup()
+{
 
   Serial.begin(9600);
   // set pins
@@ -46,93 +70,139 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(Pins::encA), readEncoder, RISING);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if (target_mode == 0){runPosControl();}
+  else if (target_mode == 1){runVelControl();}
+}
 
-
+void runPosControl()
+{
   // TO CALCULATE error
   // current time
   long t = micros();
-  // change in time 
-  float deltaT = (float)(t - t_last)/(1e-6);
+  // change in time
+  float deltaT = (float)(t - t_last) / (1e-6);
   // reset t
   t_last = t;
 
   int pos = 0;
   noInterrupts(); // disable interrupts temporarily while reading
-  pos = Motor::pos_update;
+  pos = Pos::update;
   interrupts(); // turn interrupts back on
 
   // ERROR
   int e = des - pos;
   // DERIVATIVE
-  float de_dt = (e - e_last)/(deltaT);
+  float de_dt = (e - e_last) / (deltaT);
   // INTEGRAL
-  float e_i = e_i + e*deltaT;
+  float e_i = e_i + e * deltaT;
 
   // CONTROL LOOP
-  float u = (Motor::pos_kp*e) + (Motor::pos_kd * de_dt) + (Motor::pos_ki * e_i);
+  float u = (Pos::kp * e) + (Pos::kd * de_dt) + (Pos::ki * e_i);
 
-  e_last = e; 
+  // update the error term
+  e_last = e;
 
+  // cap the pwm signal strength
   float pwr = fabs(u);
-  if (pwr > 255)
-  {
-    pwr = 255;
-  }
+  if (pwr > 255){pwr = 255;}
 
+  // if our control signal needs a negative control signal
+  // reverse the direction of the motor
   int dir = 1;
-  if (u < 0){
-    dir = -1;
-  }
+  if (u < 0){dir = -1;}
 
+  // set the motor
   setMotor(dir, pwr);
 
+  // print the results
   Serial.print(axis::lower);
   Serial.print(" ");
   Serial.print(axis::upper);
   Serial.print(" ");
   Serial.println(pos);
-
 }
 
-// void test()
-// {
+void runVelControl()
+{ 
+  int pos = 0;
+  float vel = 0;
+  noInterrupts(); // disable interrupts temporarily while reading
+  pos = Pos::update;
+  vel = Vel::update;
+  interrupts(); // turn interrupts back on
 
-//   setMotor(1, 100);
-//   delay(1000);
-//   Serial.println(Motor::pos);
+  //filter the new found velocity
+  Vel::filtered = 0.854*Vel::filtered + 0.0728*vel + 0.0728*Vel::last;
+  Vel::last = vel;
 
-//   setMotor(-1, 100);
-//   delay(1000);
-//   Serial.println(Motor::pos);
+  // Calculate error terms
+  Vel::e = des - Vel::filtered;
+  Vel::e_i = Vel::e_i + (Vel::e * Vel::deltaT);
 
-//   setMotor(0, 100);
-//   delay(1000);
-//   Serial.println(Motor::pos);
+  // calculate control signal u
+  float u = (Vel::kp * Vel::e) + (Vel::ki * Vel::e_i);
 
-// }
+  // cap the pwm signal strength
+  int pwr = fabs(u);
+  if (pwr > 255){pwr = 255;}
 
-void readEncoder() {
+  // if our control signal needs a negative control signal
+  // reverse the direction of the motor
+  int dir = 1;
+  if (u < 0){dir = -1;}
 
+  // set the motor
+  setMotor(dir, pwr);
+
+  // // print the results
+  Serial.print(axis::lower);
+  Serial.print(" ");
+  Serial.print(axis::upper);
+  Serial.print(" ");
+  Serial.println(Vel::filtered);
+  // Serial.println(Vel::update);  
+}
+
+
+void readEncoder()
+{
   int b = digitalRead(Pins::encB);
+
+  int inc = 0;
   // if b came after a, then the motor is going forward
-  if (b>0) {Motor::pos_update++;}
+  if (b > 0){inc = 1;}
   // if after, then it is going backwards
-  else {Motor::pos_update--;}
+  else{inc = -1;}
+  Pos::update = Pos::update + inc;
+
+  // only for vel control
+  // current time
+  long t = micros();
+  // change in time
+  Vel::deltaT = (float)(t - Vel::prevT) / (1e-6);
+
+  // The velocity value is how much time has elasped since the last reading
+  // (approx. manual reading) 180 counts per main shaft rev 
+  // counts / sec * (60 sec/ min) * (1 rev / 180 counts)
+  Vel::update = (inc / Vel::deltaT) / 180 * 60;
+  Vel::prevT = t; 
+
 
 }
 
-void setMotor(int dir, int pwmVal) {
+void setMotor(int dir, int pwmVal)
+{
 
   analogWrite(Pins::pwm, pwmVal);
 
-  if(dir == 1)
+  if (dir == 1)
   {
     digitalWrite(Pins::driverPin1, HIGH);
     digitalWrite(Pins::driverPin2, LOW);
   }
-  else if(dir == -1)
+  else if (dir == -1)
   {
     digitalWrite(Pins::driverPin1, LOW);
     digitalWrite(Pins::driverPin2, HIGH);
@@ -142,5 +212,4 @@ void setMotor(int dir, int pwmVal) {
     digitalWrite(Pins::driverPin1, LOW);
     digitalWrite(Pins::driverPin2, LOW);
   }
-  
 }
